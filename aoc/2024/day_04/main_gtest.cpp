c41133f2197ddaf1c33793f1f0daa2f88847d9c2
moszir <molnarszipai.richard@gmail.com>
2024-12-28 08:15:49 +0100

    add AoC day 4 frames

--- aoc/2024/day_04/main_gtest.cpp
+++ aoc/2024/day_04/main_gtest.cpp
@@ -1,8 +1,8 @@
 #include "solver.hpp"
 #include "gtest/gtest.h"
 
-#define TEST_NAME AoC2024Day25
-using Solver = aoc::year2024::day25::Solver;
+#define TEST_NAME AoC2024Day04
+using Solver = aoc::year2024::day04::Solver;
 
 using namespace std::string_literals;
 
@@ -10,26 +10,26 @@ TEST(TEST_NAME, Part1Example)
 {
     Solver solver;
     solver.parseFile("example.txt"s);
-    EXPECT_EQ("3", solver.solvePart1());
+    EXPECT_EQ("18", solver.solvePart1());
 }
 
 TEST(TEST_NAME, Part1RealInput)
 {
     Solver solver;
     solver.parseFile("input.txt"s);
-    EXPECT_EQ("3107", solver.solvePart1());
+    EXPECT_EQ("2571", solver.solvePart1());
 }
 
 TEST(TEST_NAME, Part2Example)
 {
     Solver solver;
     solver.parseFile("example.txt"s);
-    EXPECT_EQ("3", solver.solvePart2());
+    EXPECT_EQ("9", solver.solvePart2());
 }
 
 TEST(TEST_NAME, Part2RealInput)
 {
     Solver solver;
     solver.parseFile("input.txt"s);
-    EXPECT_EQ("3107", solver.solvePart2());
+    EXPECT_EQ("1992", solver.solvePart2());
 }
